c27856c3dce523353cb4be1f8c1bce18f959b60b
halchemy <harthur96@gmail.com>
2024-10-29 21:52:44 -0700

    - Created app.py with base UI

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import joblib
+
+scaler = StandardScaler()
+
+st.title("Restaurant Rating Prediction")
+
+st.set_page_config(layout="wide")
+st.caption("This app helps you predict a restaurant rating based on its features.")
+
+st.divider()
+
+average_cost = st.number_input("Please enter the estimated average cost for two", min_value=50, max_value=999999, value=1000, step=200)
+tablebooking = st.checkbox("Restaurant has table booking", ["yes", "no"])
+online_delivery = st.selectbox("Restaurant has online delivery", ["yes", "no"])
+price_range = st.selectbox("What is the price range (1 Cheapest, 4 Expensive)", [1, 2, 3, 4])
+
