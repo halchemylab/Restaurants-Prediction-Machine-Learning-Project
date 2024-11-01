1bde046ddebe131a96e34be160c6e713f1427b7c
halchemy <harthur96@gmail.com>
2024-10-31 21:10:43 -0700

    - Finished creating the app, but there are minor bugs

--- app.py
+++ app.py
@@ -17,3 +17,22 @@ tablebooking = st.checkbox("Restaurant has table booking", ["yes", "no"])
 online_delivery = st.selectbox("Restaurant has online delivery", ["yes", "no"])
 price_range = st.selectbox("What is the price range (1 Cheapest, 4 Expensive)", [1, 2, 3, 4])
 
+predict_button = st.button("Predict the rating")
+
+st.divider()
+
+model = joblib.load("model.pkl")
+
+bookingstatus = 1 if tablebooking == "Yes" else 0
+delivery = 1 if online_delivery == "Yes" else 0
+
+average_cost = scaler.fit_transform(average_cost)
+bookingstatus = scaler.fit_transform(tablebooking)
+delivery = scaler.fit_transform(online_delivery)
+price_range = scaler.fit_transform(price_range)
+
+if predict_button:
+    st.snow()
+    prediction = model.predict([[average_cost, bookingstatus, delivery, price_range]])
+
+    st.write(prediction)
